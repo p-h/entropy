a3f2615310379b724348a5f128a653681c8fc149
Philippe Hürlimann <p@hurlimann.org>
2018-07-06 12:27:32 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject entropy "0.1.0-SNAPSHOT"
+(defproject entropy "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
